12c241af99a8d009e67246bc89a489155a8a5b15
AndreyYuryev <yuryev@yandex.ru>
2024-06-07 00:46:22 +0300

    class

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('hello,world')
+
+
+class Main:
+    def __init__(self, param):
+        self.param = param
+    
+    def execute(self):
+        print(f'Значение {self.param}')
+        
+a = Main('test')
+a.execute()
